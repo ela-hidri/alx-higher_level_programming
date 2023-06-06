93c2c9b0d80ecbbec234acbc3b3d355cab825df4
ela-hidri <elahidri.eh@gmail.com>
2023-06-06 13:23:14 -0700

    fixing ussues

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for n in range(10):
     for j in range(10):
-        if n < j and n != j and n != 9 and j != 9:
-            print("{:d}{:d} ,".format(n, j), end="")
-print("{:d}{:d}".format(n, j))
+        if n < j and n != j:
+            if n == 8 and j == 9:
+                print("{:d}{:d}".format(n, j))
+            else:
+                print("{:d}{:d} ,".format(n, j), end="")
