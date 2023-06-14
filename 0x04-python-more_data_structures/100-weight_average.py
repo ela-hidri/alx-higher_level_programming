01480f9770c3f0061c4b4a3370feda96fb63e56f
ela-hidri <elahidri.eh@gmail.com>
2023-06-14 14:52:55 -0700

    adding 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    su = 0
+    div = 0
+    for i in my_list:
+        su += (i[0] * i[1])
+        div += i[1]
+    return (su / div)
