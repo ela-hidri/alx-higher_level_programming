a0ad378079c654c30d593164a454dc49fc5cf83f
ela-hidri <elahidri.eh@gmail.com>
2023-06-26 13:30:19 -0700

    adding 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    val = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[x]))
+            val += 1
+        except (TypeError, ValueError):
+            break
+        return val
