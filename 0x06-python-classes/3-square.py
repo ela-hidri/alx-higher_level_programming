2a6489fe12d611eab4d1b5c665a21b6a930e531c
ela-hidri <elahidri.eh@gmail.com>
2023-06-27 10:44:18 -0700

    adding  3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 3-square.py
+""" Define Square """
+
+
+class Square:
+    """define size with condition and calc area """
+    def __init__(self, size):
+        """ initialize attribute """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ return square area """
+        return self.__size * self.__size
