2b85ea47f11ed800bc714cec2d94571bf3fdbf9c
ela-hidri <elahidri.eh@gmail.com>
2023-06-27 10:35:23 -0700

    fixing  2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,11 +7,8 @@ class Square:
     """ difine size with condition """
     def __init__(self, size=0):
         """ initialize attribute """
-        try:
-            if size < 0:
-                raise ValueError
-            self.__size = "{:d}".format(size)
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
