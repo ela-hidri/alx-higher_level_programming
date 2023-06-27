3f7a0fd4458c55f28a81e63420c5b813dc59ccd2
ela-hidri <elahidri.eh@gmail.com>
2023-06-27 10:02:40 -0700

    create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/Python3
+# 1-square.py
+""" Difine square with size """
+
+
+class Square:
+    """ dine private attribute """
+    def __init__(self, size):
+        """ initialize attribute """
+        self.__size = size
