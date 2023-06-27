8c1b0135cbf96b8919527a23d58287d173ddee90
ela-hidri <elahidri.eh@gmail.com>
2023-06-27 15:19:50 -0700

    adding 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# 5-square.py
+""" Define Square """
+
+
+class Square:
+    """define size with condition and calc area """
+    def __init__(self, size=0):
+        """ initialize attribute """
+        self.size = size
+
+    @property
+    def size(self):
+        """ get size """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ set Size """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ return square area """
+        return (self.size * self.size)
+
+    def my_print(self):
+        """ print square """
+        for i in range(self.size):
+            for n in range(self.size):
+                print("#", end="")
+            print("")
