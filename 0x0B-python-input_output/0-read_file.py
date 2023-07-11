6f950d75221319087ea140e0a50c59f170bf90f2
ela-hidri <elahidri.eh@gmail.com>
2023-07-11 10:23:35 -0700

    adding first commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Define function
+"""
+
+
+def read_file(filename=""):
+    """
+    reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
