19d3b15a056494a8686ccd01bbcce97e18e8da0a
ela-hidri <elahidri.eh@gmail.com>
2023-07-11 12:52:01 -0700

    fixing 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,18 +2,18 @@
 """
 adds all arguments to a Python list
 """
+import os.path
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-import os.path
 path = '/usr/local/bin/'
 
+li = []
 old = []
-l = []
-if (os.path.isfile("add_item.json")): 
+if (os.path.isfile("add_item.json")):
     old = load_from_json_file("add_item.json")
 for n in old:
-    l.append(n)
+    li.append(n)
 for i in range(1, len(argv)):
-    l.append(argv[i]);
-save_to_json_file(l, "add_item.json")
+    li.append(argv[i])
+save_to_json_file(li, "add_item.json")
