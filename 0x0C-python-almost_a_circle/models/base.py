52046bfc8dce5e89a0279a8eea8fcf8f0f5aec05
ela-hidri <elahidri.eh@gmail.com>
2023-07-17 11:23:40 -0700

    adding to_json_string to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Define Base class
 """
+import json
 
 
 class Base:
@@ -11,8 +12,8 @@ class Base:
     Attributes:
         nb_objects: number of instantiated bases
     """
-
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
         Initailizing attributes
@@ -24,4 +25,12 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects 
+            self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ 
+        returns the JSON string representation of list_dictionaries:
+        """
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
