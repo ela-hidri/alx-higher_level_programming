9c43ec2ebd7c0d27ef61fa3a732dd86cf988f41f
ela-hidri <elahidri.eh@gmail.com>
2023-08-15 10:17:05 -0700

    updting 12

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-UPDATE second_table SET `score` = 10 where `name` = "Bob"
+UPDATE second_table SET `score` = 10 WHERE `name` = "Bob"
