f72e66f7b6191d7fdae3cba83567e28d0ba855d8
ela-hidri <elahidri.eh@gmail.com>
2023-09-11 14:41:25 -0700

    fixing ./11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
 const argv = process.argv;
 if (argv.length <= 3) { console.log(0); } else {
-  let max = argv[2];
-  let sdn;
-  for (let i = 3; i < argv.length; i++) {
-    if (max < argv[i]) {
-      sdn = max;
-      max = argv[i];
-    } else if (sdn == null) { sdn = argv[i]; }
-  }
-  console.log(sdn);
+  argv.sort((a, b) => a - b);
+  console.log(argv[argv.length - 2]);
 }
