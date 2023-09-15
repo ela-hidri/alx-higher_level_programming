9fb654c1a8257b9934ecb4ba0f556f3bd611f019
ela-hidri <elahidri.eh@gmail.com>
2023-09-15 13:39:11 -0700

    fixing files

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
                     .format(argv[4]))
         rows = cur.fetchall()
         for row in rows:
-            print(row)
+            if row[1] == argv[4]:
+                print(row)
         cur.close()
         db.close()
