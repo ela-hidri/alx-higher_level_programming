ff61fbdba382c58dd792949ac60eacda42ee6d48
ela-hidri <elahidri.eh@gmail.com>
2023-09-15 13:34:39 -0700

    fixing files

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ displays matching name
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    if len(argv) > 4:
+    if len(argv) >= 4:
         db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3])
         cur = db.cursor()
