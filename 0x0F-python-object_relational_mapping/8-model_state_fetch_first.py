7c7886f0e7d99777836b06fdec769b7b6308b875
ela-hidri <elahidri.eh@gmail.com>
2023-09-16 13:58:20 -0700

    fixing 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,6 +16,8 @@ if __name__ == '__main__':
 
     session = Session(engine)
     state = session.query(State).order_by(State.id).first()
-    if state != None:
+    if state is None:
+        print('Nothing')
+    else:
         print("{}: {}".format(state.id, state.name))
     session.close()
