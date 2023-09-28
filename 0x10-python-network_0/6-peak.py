cdc16241acfb43bae3c05619364e3c627ea678d9
ela-hidri <elahidri.eh@gmail.com>
2023-09-28 14:28:00 -0700

    fixing style

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" 
+"""
 define a function
 """
 
 
 def find_peak(list_of_integers):
-    """ 
+    """
     defining a function that finds a peak
     """
     if len(list_of_integers) == 0 or list_of_integers is None:
@@ -15,8 +15,8 @@ def find_peak(list_of_integers):
     while s < e:
         m = s + (e - s) // 2
         if (list_of_integers[m] > list_of_integers[m - 1] and
-            list_of_integers[m] > list_of_integers[m + 1] ):
-               return list_of_integers[m]
+           list_of_integers[m] > list_of_integers[m + 1]):
+            return list_of_integers[m]
         if list_of_integers[m - 1] > list_of_integers[m + 1]:
             e = m
         else:
