26ccf7a4565bc46051837eeb9ccabd4678363e25
ela-hidri <elahidri.eh@gmail.com>
2023-09-29 15:39:48 -0700

    adding 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+akes your GitHub credentials (username and password) and
+uses the GitHub API to display your id
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    r = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
+    print(r.json().get('id'))
