9f3ecd22f9a22021d5c2b36874238bc195736f95
ela-hidri <elahidri.eh@gmail.com>
2023-10-24 11:17:07 -0700

    adding 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = process.argv;
+const request = require('request');
+request(argv[2], function (error, response, body) {
+  console.log('code: ', response && response.statusCode);
+  if (error) {
+    console.error(error);
+  }
+});
