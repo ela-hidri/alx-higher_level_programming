fbae0be6e154918bdd7ac1dd8d36379971882ae8
ela-hidri <elahidri.eh@gmail.com>
2023-10-24 13:38:57 -0700

    adding 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 request('https://swapi-api.alx-tools.com/api/films/' + argv[2], function (error, response, body) {
   const ch = JSON.parse(body).characters;
   ch.forEach(c => {
-    request(c, function (error, response, body) {
+    request(c, async function (error, response, body) {
       const name = JSON.parse(body).name;
       console.log(name);
       if (error) {
